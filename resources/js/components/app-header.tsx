b963026717fc62661fe843ec448bbc977ba768b4
larry <songjiellarry@gmail.com>
2026-04-15 22:35:45 -0700

    update link url

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -50,12 +50,12 @@ const mainNavItems: NavItem[] = [
 const rightNavItems: NavItem[] = [
     {
         title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
+        href: 'https://github.com/emphs/p3975',
         icon: Folder,
     },
     {
         title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
+        href: 'https://deepwiki.com/emphs/p3975',
         icon: BookOpen,
     },
 ];
